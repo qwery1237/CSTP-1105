35ac9513d345ca6c3256292673814f2ff255006c
qwery1237 <jinsu4768@gmail.com>
2022-07-07 17:15:38 -0700

    df

--- self/function.js
+++ self/function.js
@@ -3,5 +3,8 @@ const showMessage = (message, from = 'unknown') => log(`${message} by ${from}`);
 showMessage('hi','jinsoo');
 showMessage('hi');
 
-const printAll = (...arr) => log(arr);
-printAll = 
+const printAll = (a,b,...arr) => log(a,b,...arr);
+printAll(1,2,[3,4,5],4,5);
+log(1,2,3,4,5);
+log(1,2,[3,4,5]);
+console.log(1,2,[3,4,5]);
