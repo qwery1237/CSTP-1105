a77b3751ee4212038bf43ddd8adb12315d719c74
qwery1237 <jinsu4768@gmail.com>
2022-08-08 17:48:20 -0700

    Create calc.py

---
+++ python_folder/calc.py
@@ -0,0 +1,25 @@
+# Calculator
+# # Info on Tkinter
+# https://realpython.com/python-gui-tkinter/
+
+# Import only Tk (interface), Widgets - Entry (text input) and Buttons
+
+
+# Interface Variables
+
+
+
+# Build Graphical User Interface
+# Text Input
+
+
+# Buttons
+# Numbers
+
+
+
+# Interface functions
+
+
+# Set everything up
+
